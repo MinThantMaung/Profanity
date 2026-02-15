c4703467859e55511f7796b074649e60386bc12f
Min Thant Mg <minthant180@gmail.com>
2026-02-16 00:39:16 +0900

    feat: initialized settings

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+
+const app = express();
+app.use(express.json());
+
+app.get('/check', (req, res) => {
+  res.send('Server is running');
+});
+
+const PORT = 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
